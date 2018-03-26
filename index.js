11e2d8754ac32dad404bf61b9f0ee80528ce9b80
Renee Serraglio <rserraglio@student.unimelb.edu.au>
2018-03-26 12:35:12 +1100

    changed port backup

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function(req, res) {
     res.send('Hello');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT,function() {
     console.log(`Express listening on port ${PORT}`);
 });
